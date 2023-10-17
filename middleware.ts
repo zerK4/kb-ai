aea73e7353a86a253d7ef2ccefe1bc54c6af8ce6
zerK4 <sebastian.v.pavel@gmail.com>
2023-10-17 09:23:27 +0300

    update middleware

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,16 @@
-import { authMiddleware } from '@clerk/nextjs';
-
-// This example protects all routes including api/trpc routes
-// Please edit this to allow other routes to be public as needed.
-// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
-export default authMiddleware({});
-
-export const config = {
-  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
-};
+import { authMiddleware, redirectToSignIn } from '@clerk/nextjs';
+import { NextResponse } from 'next/server';
+ 
+export default authMiddleware({
+  afterAuth(auth, req, evt) {
+    // handle users who aren't authenticated
+    if (!auth.userId && !auth.isPublicRoute) {
+      return redirectToSignIn({ returnBackUrl: req.url });
+    }
+    // redirect them to organization selection page
+    if(auth.userId && !auth.orgId && req.nextUrl.pathname !== "/"){
+      const orgSelection = new URL('/', req.url)
+      return NextResponse.redirect(orgSelection)
+    }
+  }
+});
